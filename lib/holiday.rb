f5fdd7b37e9407cb55219c518cc24d42e414c2a7
Donna Torres <todonnatorres@icloud.com>
2020-06-06 20:36:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,7 @@ end
 def add_supply_to_winter_holidays(holiday_hash, supply)
   
    holiday_hash[:winter][:christmas] << supply
- holiday_hash[:winter][:new_years] << supply
+  holiday_hash[:winter][:new_years] << supply
  
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
@@ -35,6 +35,8 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+  
+  
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
